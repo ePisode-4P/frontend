25a0a93a24d938476ef334920b5fac902cc739c2
jindayeong <jindy0423@gmail.com>
2024-05-10 23:51:43 +0900

    Update - 카테고리 별 이미지 추가

--- ePisode/src/components/Diary/Diary.jsx
+++ ePisode/src/components/Diary/Diary.jsx
@@ -27,6 +27,51 @@ export default function Diary({ selectedPlace, setSelectedPlace }) {
     setLoved(!loved)
   }
 
+  const categoryGroups = {
+    학교: ['초등학교', '중학교', '고등학교', '대학교', '특수학교', '특목고등학교'],
+    영화관: ['CGV', '메가박스', '롯데시네마', '아파트'],
+    카페: ['카페', '커피전문점', '디저트카페', '갤러리카페', '제과,베이커리', '스타벅스'],
+    역: ['기차역', 'KTX,SRT정차역', '공항'],
+    병원: ['병원', '대학병원', '종합병원', '외과', '내과', '약국'],
+    마트: ['이마트', '롯데마트', '홈플러스'],
+    백화점: ['백화점', '롯데백화점', '현대백화점'],
+    공원: ['공원', '도시근린공원'],
+  };
+
+  const getCategoryStyle = () => {
+    const category = categoryName.trim();
+
+    if (categoryGroups['학교'].includes(category)) {
+      return styles.school;
+    }
+    else if (categoryGroups['영화관'].includes(category)) {
+      return styles.movie;
+    }
+    else if (categoryGroups['카페'].includes(category)) {
+      return styles.cafe;
+    }
+    else if (categoryGroups['역'].includes(category)) {
+      return styles.station;
+    }
+    else if (categoryGroups['병원'].includes(category)) {
+      return styles.hospital;
+    }
+    else if (categoryGroups['마트'].includes(category)) {
+      return styles.mart;
+    }
+    else if (categoryGroups['백화점'].includes(category)) {
+      return styles.department;
+    }
+    else if (categoryGroups['공원'].includes(category)) {
+      return styles.park;
+    }
+    else {
+      return styles.default;
+    }
+  };
+
+  const imageStyle = `${styles.image} ${getCategoryStyle()}`;
+
   return (
     <motion.div
       style={{
@@ -45,7 +90,7 @@ export default function Diary({ selectedPlace, setSelectedPlace }) {
       <button className={styles.close_diary} onClick={handleCloseClick}>
         <IoCloseOutline />
       </button>
-      <section className={styles.image}></section>
+      <section className={imageStyle}></section>
       <section className={styles.place_info}>
         <div className={styles.wrap}>
           <div className={styles.wrap_name}>
