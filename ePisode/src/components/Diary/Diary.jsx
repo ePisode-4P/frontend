c26dc5f9de300c34ca18ba160887aec418df1934
jindayeong <jindy0423@gmail.com>
2024-05-13 23:03:31 +0900

    Update - 카테고리 이미지 추가

--- ePisode/src/components/Diary/Diary.jsx
+++ ePisode/src/components/Diary/Diary.jsx
@@ -38,14 +38,16 @@ export default function Diary({ selectedPlace, setSelectedPlace }) {
   }
 
   const categoryGroups = {
-    학교: ['초등학교', '중학교', '고등학교', '대학교', '특수학교', '특목고등학교'],
-    영화관: ['CGV', '메가박스', '롯데시네마', '아파트'],
+    학교: ['초등학교', '중학교', '고등학교', '대학교', '전문대학', '특수학교', '특목고등학교', '특성화고등학교'],
+    영화관: ['CGV', '메가박스', '롯데시네마', '아파트', '어린이집', '유치원'],
     카페: ['카페', '커피전문점', '디저트카페', '갤러리카페', '제과,베이커리', '스타벅스'],
     역: ['기차역', 'KTX,SRT정차역', '공항'],
-    병원: ['병원', '대학병원', '종합병원', '외과', '내과', '약국'],
-    마트: ['이마트', '롯데마트', '홈플러스'],
+    병원: ['병원', '대학병원', '종합병원', '외과', '내과', '약국', '피부과'],
+    마트: ['이마트', '롯데마트', '홈플러스', 'GS25', 'CU'],
     백화점: ['백화점', '롯데백화점', '현대백화점'],
     공원: ['공원', '도시근린공원'],
+    정류장: ['고속,시외버스정류장'],
+    박물관: ['박물관', '미술관'],
   };
 
   const getCategoryStyle = () => {
@@ -74,6 +76,12 @@ export default function Diary({ selectedPlace, setSelectedPlace }) {
     }
     else if (categoryGroups['공원'].includes(category)) {
       return styles.park;
+    }
+    else if (categoryGroups['정류장'].includes(category)) {
+      return styles.busstop;
+    }
+    else if (categoryGroups['박물관'].includes(category)) {
+      return styles.museum;
     }
     else {
       return styles.default;
